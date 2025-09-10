1b6f2363fd0e680f6dbbcc4245222c39d1acebce
Abril <herbasabril4@gmail.com>
2025-09-10 17:14:16 -0300

    Cree la primera grilla de productos a terminar.

---
+++ Homy/src/components/grilla/grilla.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./grilla.css"; 
+
+const products = [
+  {
+    id: 1,
+    name: "Cajon de 2 puertas",
+    price: 198500,
+    description: "Cajon Blanco 2 puertas 80x120cm."
+  },
+  {
+    id: 2,
+    name: "Mesa de Luz",
+    price: 135000,
+    description: "Mesa de Luz Gris Oscuro 60x100cm."
+  },
+  {
+    id: 3,
+    name: "Placard 4 puertas",
+    price: 250600,
+    description: "Placard de 4 puertas gris con blanco"
+  }
+];
+
+const ProductList = () => {
+  return (
+    <div className="product-container">
+      <h1>Lista de Productos</h1>
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <h2>{product.name}</h2>
+          <p><strong>Precio:</strong> ${product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
